51746aebda4beaa795ae4eb59d872bfb277edd32
lpilot <lorenz.pilot@smail.th-koeln.de>
2023-07-06 11:27:46 +0200

    added working motors for arduino nano

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,18 @@
 #include "I2Cdev.h"
 
 #include "MPU6050_6Axis_MotionApps20.h"
-#include "MPU6050.h" // not necessary if using MotionApps include file
+//#include "MPU6050.h" // not necessary if using MotionApps include file
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
-#include "Servo.h"
-//#define _BV(bit) (1 << (bit))
+#include <Servo.h>
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-//AD0 high = 0x69
+// AD0 high = 0x69
 MPU6050 mpu;
 //MPU6050 mpu(0x69); // <-- use for AD0 high
 
@@ -85,11 +84,11 @@ void setup() {
   // initialize serial communication
   // (115200 chosen because it is required for Teapot Demo output, but it's
   // really up to you depending on your project)
-  Serial.begin(38400);
+  Serial.begin(115200);
   while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
   // initialize device
-  //Serial.println(F("Initializing I2C devices..."));
+  Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
   pinMode(INTERRUPT_PIN, INPUT);
   devStatus = mpu.dmpInitialize();
@@ -125,9 +124,11 @@ void setup() {
   }
 
   // Define the pins to which the 3 servo motors are connected
-  servo0.attach(12);
+  servo0.attach(10);
   servo1.attach(9);
   servo2.attach(8);
+  Serial.print(devStatus);
+  Serial.print("Hallo!!!!!");
 }
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
@@ -177,6 +178,8 @@ void loop() {
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
+    Serial.print(ypr[0]);
+
     // Yaw, Pitch, Roll values - Radians to degrees
     ypr[0] = ypr[0] * 180 / M_PI;
     ypr[1] = ypr[1] * 180 / M_PI;
@@ -197,6 +200,7 @@ void loop() {
       
       // Control the servos according to the MPU6050 orientation
       servo0.write(servo0Value);
+      Serial.println(servo0Value);
       servo1.write(servo1Value);
       servo2.write(servo2Value);
     }
